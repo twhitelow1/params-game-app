870835976599e56ffb9a197bf8577239fc3a6304
Todd Whitelow <todd@whitelow.tech>
2020-09-01 20:21:38 -0600

    set variables

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,2 +1,7 @@
 class Api::GamesController < ApplicationController
+  def games_action
+    url_input = params["name"]
+    @output_message = "This is your name: #{url_input}"
+    render "games.json.jb"
+  end
 end
